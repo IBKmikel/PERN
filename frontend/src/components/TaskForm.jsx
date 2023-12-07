bc49c1cf71802a07212f1bfd23d68c45b745972e
IBKmikel <mikel.ibkc@gmail.com>
2023-12-07 16:14:54 -0600

    finalización de crud entre react a node

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,6 +1,6 @@
 import { Button, Card, CardContent, CircularProgress, Grid, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function TaskForm(){
     const [task, setTask] = useState({
@@ -9,23 +9,32 @@ export default function TaskForm(){
     });
 
     const [loading, setLoading] = useState(false);
+    const [editing, setEditing] = useState(false);
 
     const history = useNavigate();
+    const params = useParams();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         setLoading(true);
-        
-        const response = await fetch('http://localhost:3000/tasks', {
-            method: 'POST',
-            body: JSON.stringify(task),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
 
-        const data = await response.json();
+        if(editing){
+            await fetch(`http://localhost:3000/tasks/${params.id}`,{
+                method: 'PUT',
+                body: JSON.stringify(task),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            });
+        }else{
+            await fetch('http://localhost:3000/tasks', {
+                method: 'POST',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }
         setLoading(false);
         history('/');
     }
@@ -34,11 +43,26 @@ export default function TaskForm(){
         setTask({ ...task, [e.target.name]: e.target.value });
     }
 
+    const loadTask = async(id) =>{
+        setEditing(true);
+        const response = await fetch(`http://localhost:3000/tasks/${id}`);
+        const data = await response.json();
+        setTask({title: data[0].title, description: data[0].description});
+    }
+
+    useEffect(() =>{
+        if(params.id){
+            loadTask(params.id);
+        }
+    }, [params.id]);
+
     return(
         <Grid container direction='column' alignItems='center' justifyContent='center'>
             <Grid item xs={3}>
                 <Card sx={{mt:5}} style={{backgroundColor: '#1e272e', padding: '1rem'}}>
-                    <Typography variant="5" textAlign='center' color='white'>Create Task</Typography>
+                    <Typography variant="5" textAlign='center' color='white'>
+                        {editing ? "Edit Task" : "Create Task"}    
+                    </Typography>
                         <CardContent>
                             <form onSubmit={handleSubmit}>
                                 <TextField 
@@ -46,6 +70,7 @@ export default function TaskForm(){
                                     label="Write your title" 
                                     sx={{display: 'block', margin: '.5rem 0'}}
                                     name='title'
+                                    value={task.title}
                                     onChange={handleChange}
                                     inputProps={{style: {color:'white'}}} 
                                     InputLabelProps={{style:{color:'white'}}} 
@@ -57,6 +82,7 @@ export default function TaskForm(){
                                     rows={4} 
                                     sx={{display: 'block', margin: '.5rem 0'}}
                                     name="description"
+                                    value={task.description}
                                     onChange={handleChange} 
                                     inputProps={{style: {color:'white'}}} 
                                     InputLabelProps={{style:{color:'white'}}} 
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,14 +1,29 @@
 import { Button, Card, CardContent, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function TaskList(){
     const [tasks, setTasks] = useState([]);
+
+    const history = useNavigate();
 
     const loadTasks = async() =>{
         const response = await fetch('http://localhost:3000/tasks');
         const data = await response.json();
         
         setTasks(data);
+    }
+
+    const handleDelete = async(id)=>{
+        try {
+            await fetch(`http://localhost:3000/tasks/${id}`, {
+                method: "DELETE",
+            });
+
+            setTasks(tasks.filter(task => task.id !== id));
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     useEffect(() =>{
@@ -20,7 +35,8 @@ export default function TaskList(){
             <h1>Task List</h1>
             {
                 tasks.map((task) => (
-                    <Card key={task.title} style={{
+                    <Card key={task.id} 
+                        style={{
                         marginBottom: '.7rem',
                         backgroundColor: '#1e272e'
                     }}>
@@ -33,10 +49,10 @@ export default function TaskList(){
                                 <Typography>{task.description}</Typography>
                             </div>
                             <div>
-                                <Button variant="contained" color="inherit" onClick={() => console.log('edit')}>
+                                <Button variant="contained" color="inherit" onClick={() => history(`/tasks/${task.id}/edit`)}>
                                     Edit
                                 </Button>
-                                <Button variant="contained" color='warning' onClick={() => console.log('delete')} style={{marginLeft: '.5rem'}}>
+                                <Button variant="contained" color='warning' onClick={() => handleDelete(task.id)} style={{marginLeft: '.5rem'}}>
                                     Delete
                                 </Button>
                             </div>
